e87604a43c5cbf63e6569457b90a0aaa27f34c7b
William Cullum <william-cullum@hotmail.co.uk>
2023-10-11 19:45:18 +0100

    still struggling. I know I need to move the window and keep lowest, left and highest right, but ake note of make profit, btu what If I come across a value lower than my original and I dont know the values to the right so I dont know what my max profit is? I guess I update the lowest found anyway, and if no change in mp then there's no change and we know the max profit either way

--- Leet/arrays/121.py
+++ Leet/arrays/121.py
@@ -2,26 +2,59 @@
     # I think you need to identify the lowest number, and then slice the list
     #find the value with the largest difference (profit) between the lowest 
     #number and itself
-def maxProfit(prices: list[int]) -> int:
+# def maxProfit(prices: list[int]) -> int:
     
-    lowest_val = min(prices)
-    print(f'lowest value is: {lowest_val}')
+#     lowest_val = min(prices)
+#     print(f'lowest value is: {lowest_val}')
 
-    lowest_idx = prices.index(lowest_val)
+#     lowest_idx = prices.index(lowest_val)
 
-    new_prices = prices[lowest_idx:]
-    print(new_prices)
+#     new_prices = prices[lowest_idx:]
+#     print(new_prices)
     
-    most_profit = 0
+#     most_profit = 0
 
-    for i in range(len(new_prices)):
-        if (new_prices[i] - new_prices[0]) > most_profit:
-            most_profit = new_prices[i] - new_prices[0]
-        else:
-            continue
+#     for i in range(len(new_prices)):
+#         if (new_prices[i] - new_prices[0]) > most_profit:
+#             most_profit = new_prices[i] - new_prices[0]
+#         else:
+#             continue
         
-    print(most_profit)
+#     print(most_profit)
+
+def maxProfit(prices: list[int]) -> int:
+
+    start = prices[0]
+    end = 0
+    mp = 0
+    lowest = start
+
+    while end < len(prices):
+
+        if prices[end] < lowest:
+            lowest = prices[end]
+            end += 1
+        if (prices[end] - lowest) > mp:
+            mp = (prices[end] - lowest) 
 
 
-price = [4,3,5,3,1,2,3]
+
+        # if (prices[end] - start) <= mp:
+        #     end += 1
+        # elif (prices[end] - start) > mp:
+        #     mp = (prices[end] - start)
+        #     end += 1  
+
+    print(mp)
+
+
+
+
+
+
+price = [4,3,5,3,1,2,10]
 maxProfit(price)
+
+'''
+- Expand the window and determine if the profit is larger than before 
+'''
