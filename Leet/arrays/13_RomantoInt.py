b760d3b0d1ad0026f047e4aa5fa9ed58394baa98
William Cullum <william-cullum@hotmail.co.uk>
2023-10-11 20:55:34 +0100

    not working because when the roman element is singular then its not capturing the last element in the string becasue of the way I'm defining IV as 9 for example and its either not including last element or its going over the array. Need to find a way fo qualifying what number it is without defining everything

---
+++ Leet/arrays/13_RomantoInt.py
@@ -0,0 +1,25 @@
+
+
+def dank (s: str):
+    roman = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
+    total = 0
+    for i in range(len(s) - 1):
+        if roman[s[i]] < roman[s[i + 1]]:
+            value = roman[s[i + 1]] - roman[s[i]]
+            total += value
+        else:
+            value = roman[s[i]]
+            total += value
+    print(total)
+
+
+dank('III')
