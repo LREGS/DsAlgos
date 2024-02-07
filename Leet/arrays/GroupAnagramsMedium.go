5d64dc61d75a7be2934e8e0a4464ac7ffd4086e7
william <william-cullum@hotmail.co.uk>
2024-02-07 22:00:41 +0000

    first leet code medium :)

---
+++ Leet/arrays/GroupAnagramsMedium.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func groupAnagrams(strs []string) [][]string {
+
+	anagramMap := make(map[string][]string)
+
+	for _, str := range strs {
+		sortedString := SortString(str)
+
+		_, ok := anagramMap[sortedString]
+		if ok {
+			anagramMap[sortedString] = append(anagramMap[sortedString], str)
+		} else {
+			anagramMap[sortedString] = append(anagramMap[sortedString], str)
+		}
+	}
+
+	response := make([][]string, 0)
+	for key := range anagramMap {
+		response = append(response, anagramMap[key])
+	}
+
+	return response
+}
+
+func SortString(input string) string {
+	runes := []rune(input)
+
+	sort.Slice(runes, func(i, j int) bool {
+		return runes[i] < runes[j]
+	})
+
+	sortedString := string(runes)
+
+	return sortedString
+}
+
+func main() {
+	anagrams := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+	sorted := groupAnagrams(anagrams)
+	fmt.Println(sorted)
+}
