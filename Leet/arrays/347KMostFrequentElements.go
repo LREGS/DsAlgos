45012ef280132bdddae48922f79588f82cdefdeb
william <william-cullum@hotmail.co.uk>
2024-02-12 18:21:17 +0000

    second medium :D

--- Leet/arrays/347KMostFrequentElements.go
+++ Leet/arrays/347KMostFrequentElements.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 //  """Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
@@ -17,7 +18,7 @@ import (
 
 //same as last problem - add all the elements into a dictionary, return all elements from the dictionary into a list, sort the list, return top 2 elements
 
-func topKFrequent(nums []int) {
+func topKFrequent(nums []int, k int) []int {
 
 	tokenMap := make(map[int][]int)
 
@@ -25,14 +26,37 @@ func topKFrequent(nums []int) {
 		tokenMap[num] = append(tokenMap[num], num)
 	}
 
+	answers := make([][]int, 0)
+
 	for _, value := range tokenMap {
-		fmt.Println(len(value))
+		answers = append(answers, value)
 	}
+
+	lessFunc := func(i, j int) bool {
+		return len(answers[j]) < len(answers[i])
+	}
+
+	sort.Slice(answers, lessFunc)
+
+	selected := answers[:k]
+
+	actualAnswer := make([]int, 0)
+
+	for _, value := range selected {
+		if len(actualAnswer) == k {
+			break
+		}
+		actualAnswer = append(actualAnswer, value[1])
+
+	}
+
+	return actualAnswer
 
 }
 
 func main() {
-	nums := []int{1, 2, 3, 4, 5, 6, 7, 7, 7, 8, 8, 8}
-	topKFrequent(nums)
+	nums := []int{1, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6, 6, 7, 7, 7, 8, 8, 8}
+	answer := topKFrequent(nums, 3)
+	fmt.Println(answer)
 
 }
---
+++ Leet/arrays/dank.py
@@ -0,0 +1,3 @@
+import danker
+
+print(danker.variable)
