4d9e4421123e18586453e1ce710e04d431a81c2e
william <william-cullum@hotmail.co.uk>
2024-02-14 11:34:32 +0000

    jesus wept spent ages trying to figure why it did not work and I was comparing if there was a duplicate after only adding one element, and comparing to the element I just added lol

---
+++ Leet/arrays/checkDuplicates.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func isValidSudoko(board [][]byte) bool {
+	squares := make(map[string][]byte)
+
+	columns := make(map[int][]byte)
+	rows := make(map[int][]byte)
+
+	for row, line := range board {
+		for column, value := range line {
+			//this is the value of an empty string
+			if value == '.' {
+				continue
+			}
+
+			if ContainsDuplicate(columns[column], value) {
+				fmt.Println(columns[column], value)
+				return false
+			}
+			columns[column] = append(columns[column], value)
+
+			if ContainsDuplicate(rows[row], value) {
+				fmt.Println(rows[row], value)
+				return false
+			}
+			rows[row] = append(rows[row], value)
+
+			grid := fmt.Sprintf(strconv.Itoa(row/3) + strconv.Itoa(column/3))
+			if ContainsDuplicate(squares[grid], value) {
+				return false
+			}
+			squares[grid] = append(squares[grid], value)
+
+		}
+	}
+
+	return true
+}
+
+func ContainsDuplicate(s []byte, v byte) bool {
+	for _, value := range s {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	board := [][]byte{
+		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '5', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+	}
+	err := isValidSudoko(board)
+	if err != true {
+		fmt.Println("invalid")
+	} else {
+		fmt.Println("valid")
+	}
+}
--- Leet/arrays/validSudoko.go
+++ Leet/arrays/validSudoko.go
@@ -1,123 +1,143 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-//could use bucket sort to sort the numbers in each square and then sort through each column at the same time
-//if a duplicate is found return fasle
+// //could use bucket sort to sort the numbers in each square and then sort through each column at the same time
+// //if a duplicate is found return fasle
 
-func isValidSudoko(board [][]byte) bool {
+// func isValidSudoko(board [][]byte) bool {
 
-	//new square every 3 index
-	squares := make(map[int][]byte)
+// 	//new square every 3 index
+// 	squares := make(map[int][]byte)
 
-	//column = i
-	column := make(map[int][]byte)
+// 	//column = i
+// 	column := make(map[int][]byte)
+// 	row := make(map[int][]byte)
 
-	// currentSquare := ""
+// 	// currentSquare := ""
 
-	for i, line := range board {
-		for j, value := range line {
-			if value == 46 {
-				continue
-			}
-			err := checkDuplicate(j, value, column)
-			if err != true {
-				return false
-			}
-			column[j] = append(column[j], value)
+// 	for i, line := range board {
+// 		for j, value := range line {
+// 			if value == 46 {
+// 				continue
+// 			}
 
-			switch {
-			case i <= 2 && j <= 2:
-				err := checkDuplicate(i+1, value, column)
-				if err != true {
-					return false
-				}
-				squares[1] = append(squares[1], value)
-			case i <= 2 && 2 < j && j < 6:
-				err := checkDuplicate(i+1, value, column)
-				if err != true {
-					return false
-				}
-				squares[2] = append(squares[2], value)
-			case i <= 2 && 6 <= j && j <= 8:
-				err := checkDuplicate(i+1, value, column)
-				if err != true {
-					return false
-				}
-				squares[3] = append(squares[3], value)
-			case 2 < i && i < 6 && j <= 2:
-				err := checkDuplicate(i+1, value, column)
-				if err != true {
-					return false
-				}
-				squares[4] = append(squares[4], value)
-			case 2 < i && i < 6 && 2 < j && j < 6:
-				err := checkDuplicate(i+1, value, column)
-				if err != true {
-					return false
-				}
-				squares[5] = append(squares[5], value)
-			case 2 < i && i < 6 && 6 <= j && j <= 8:
-				err := checkDuplicate(i+1, value, column)
-				if err != true {
-					return false
-				}
-				squares[6] = append(squares[6], value)
-			case 6 <= i && i <= 9 && j <= 2:
-				err := checkDuplicate(i+1, value, column)
-				if err != true {
-					return false
-				}
-				squares[7] = append(squares[7], value)
-			case 6 <= i && i <= 8 && 2 < j && j < 6:
-				err := checkDuplicate(i+1, value, column)
-				if err != true {
-					return false
-				}
-				squares[8] = append(squares[8], value)
-			case 6 <= i && i <= 8 && 6 <= j && j <= 8:
-				err := checkDuplicate(i+1, value, column)
-				if err != true {
-					return false
-				}
-				squares[9] = append(squares[9], value)
-			}
+// 			column[j] = append(column[j], value)
+// 			err := checkDuplicate(j, value, column)
+// 			fmt.Println(err)
+// 			if err != true {
+// 				fmt.Println(column)
+// 				return false
+// 			}
+// 			//forgot to make one for rows
 
-		}
+// 			row[i] = append(row[i], value)
+// 			err = checkDuplicate(i, value, row)
+// 			if err != true {
+// 				fmt.Println(row)
+// 				return false
+// 			}
 
-	}
-	fmt.Println(squares, column)
-	return true
+// 			// this can be made simpler with integer division
+// 			switch {
+// 			case i <= 2 && j <= 2:
+// 				err := checkDuplicate(i+1, value, squares)
+// 				if err != true {
+// 					fmt.Println("3")
+// 					return false
+// 				}
+// 				squares[1] = append(squares[1], value)
+// 			case i <= 2 && 2 < j && j < 6:
+// 				err := checkDuplicate(i+1, value, squares)
+// 				if err != true {
+// 					fmt.Println("4")
+// 					return false
+// 				}
+// 				squares[2] = append(squares[2], value)
+// 			case i <= 2 && 6 <= j && j <= 8:
+// 				err := checkDuplicate(i+1, value, squares)
+// 				if err != true {
+// 					return false
+// 				}
+// 				squares[3] = append(squares[3], value)
+// 			case 2 < i && i < 6 && j <= 2:
+// 				err := checkDuplicate(i+1, value, squares)
+// 				if err != true {
+// 					return false
+// 				}
+// 				squares[4] = append(squares[4], value)
+// 			case 2 < i && i < 6 && 2 < j && j < 6:
+// 				err := checkDuplicate(i+1, value, squares)
+// 				if err != true {
+// 					return false
+// 				}
+// 				squares[5] = append(squares[5], value)
+// 			case 2 < i && i < 6 && 6 <= j && j <= 8:
+// 				err := checkDuplicate(i+1, value, squares)
+// 				if err != true {
+// 					return false
+// 				}
+// 				squares[6] = append(squares[6], value)
+// 			case 6 <= i && i <= 9 && j <= 2:
+// 				err := checkDuplicate(i+1, value, squares)
+// 				if err != true {
+// 					return false
+// 				}
+// 				squares[7] = append(squares[7], value)
+// 			case 6 <= i && i <= 8 && 2 < j && j < 6:
+// 				err := checkDuplicate(i+1, value, squares)
+// 				if err != true {
+// 					return false
+// 				}
+// 				squares[8] = append(squares[8], value)
+// 			case 6 <= i && i <= 8 && 6 <= j && j <= 8:
+// 				err := checkDuplicate(i+1, value, squares)
+// 				if err != true {
+// 					return false
+// 				}
+// 				squares[9] = append(squares[9], value)
+// 			}
+// 			continue
 
-}
+// 		}
 
-func checkDuplicate(k int, v byte, m map[int][]byte) bool {
-	for _, value := range m[k] {
-		if value == v {
-			return false
-		}
-	}
-	return true
-}
+// 	}
+// 	fmt.Println(squares, column)
+// 	return true
 
-func main() {
-	board := [][]byte{
-		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
-	}
-	err := isValidSudoko(board)
-	if err != false {
-		fmt.Println("valid sudoko")
-	} else {
-		fmt.Println("invalid sudoko")
-	}
-}
+// }
+
+// func checkDuplicate(k int, v byte, m map[int][]byte) bool {
+// 	if len(m) < 1 {
+// 		return true
+// 	}
+// 	for _, value := range m[k] {
+// 		if value == v {
+// 			fmt.Println(value)
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+// func main() {
+// 	board := [][]byte{
+// 		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+// 		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+// 		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+// 		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+// 		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+// 		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+// 		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+// 		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+// 		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+// 	}
+// 	err := isValidSudoko(board)
+// 	if err != false {
+// 		fmt.Println("valid sudoko")
+// 	}
+// 	fmt.Println("invalid sudoko")
+
+// }
