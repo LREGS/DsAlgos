b6517b0e4404c9cab771b81db6e0e75239b194c8
william <william-cullum@hotmail.co.uk>
2024-08-29 19:11:01 +0100

    some progress on minStack

---
+++ stack/minStack.go
@@ -0,0 +1,49 @@
+package stack
+
+type MinStack struct {
+	stack      [][]int // value, minimum
+	CurrentMin int
+}
+
+func Constructor() MinStack {
+	return MinStack{stack: make([][]int, 0)}
+}
+
+func (s *MinStack) Push(sToAdd int) {
+	if !(len(s.stack) > 0) {
+		s.CurrentMin = sToAdd
+	}
+	if sToAdd <= s.CurrentMin {
+		s.stack = append(s.stack, []int{sToAdd, sToAdd})
+		s.CurrentMin = sToAdd
+	} else {
+		s.stack = append(s.stack, []int{sToAdd, s.CurrentMin})
+	}
+
+}
+
+func (s *MinStack) Pop() {
+	if len(s.stack) > 0 {
+		s.stack = s.stack[:len(s.stack)-1]
+	}
+}
+
+func (s *MinStack) FullTop() []int {
+	if len(s.stack) > 0 {
+		return s.stack[len(s.stack)-1]
+	} else {
+		return nil
+	}
+}
+
+func (s *MinStack) Top() int {
+	if len(s.stack) > 0 {
+		return s.stack[len(s.stack)-1][0]
+	} else {
+		return -1
+	}
+}
+
+func (s *MinStack) GetMin() int {
+	return s.FullTop()[1]
+}
